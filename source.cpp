4ff39f351be615c192d98104a73db21eeb1b0c2e
Максим <56740030+Maxim1720@users.noreply.github.com>
2019-12-18 14:11:04 +0400

    Add files via upload
    laba7_1

---
+++ source.cpp
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<string.h>
+#include "library/palindroms.h"
+
+int main() {
+
+	FILE* source, * target;
+	char str[100], new_str[100];
+	char name1[] = "C:\\Users\\son of chemistry\\OneDrive - ФГБОУ ВО ПГУТИ\\Программирование\\Лаба7\\laba7_1\\Files\\text.txt";
+	char name2[] = "C:\\Users\\son of chemistry\\OneDrive - ФГБОУ ВО ПГУТИ\\Программирование\\Лаба7\\laba7_1\\Files\\palindroms.txt";
+	source = fopen(name1, "w");
+	int count = 0;
+	if (source != NULL) {
+		while (*str != '0') {
+			fgets(str, 100, stdin);
+			if (*str != '0') {
+				fputs(str, source);
+				count++;
+			}
+			
+		}
+		fclose(source);
+	}
+	else
+		return 1;
+
+	source = fopen(name1, "r");
+	target = fopen(name2, "w");if (target == NULL)printf("file hasn't opened");
+	
+	for (int i = 0;i < count;i++){
+		fgets(new_str, 100, source);
+		if (palindroms(new_str) == 1) {  
+			fputs(new_str, target);
+		}
+	}
+	fclose(source);
+	fclose(target);
+}
+
